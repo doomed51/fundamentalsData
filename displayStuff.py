46adc7b4e3392bea54bcf1b562938d1174c5ee14
doomed51 <doomed51@gmail.com>
2019-02-10 01:38:11 +0100

    switched back to PySimpleGUI from PySimpleGUIWx

--- displayStuff.py
+++ displayStuff.py
@@ -1,4 +1,4 @@
-import PySimpleGUIWx as sg      
+import PySimpleGUI as sg      
 
 import ticker
 import api_IEX
@@ -15,30 +15,35 @@ TableLayoutWx = [
             #           Basics
             # ****************************
             [sg.Text('Basics', size=(45,2), justification='center')],
-            [sg.Text('Market Cap', size=(15, 1)), sg.Text(str(myTicker.marketCap),size=(15,1))],
-            [sg.Text('Latest Price', size=(15, 1)), sg.Text(str(myTicker.latestPrice),size=(15,1))],
-            [sg.Text('52 Week Low', size=(15, 1)), sg.Text(str(myTicker.week52Low),size=(15,1))],
-            [sg.Text('52 Week High', size=(15, 1)), sg.Text(str(myTicker.week52High),size=(15,1))],
-            [sg.Text('Total Revenue', size=(15, 1)), sg.Text(str(myTicker.revenue),size=(15,2))],
+            [sg.Text('Market Cap', size=(15, 1)), sg.Text(myTicker.marketCap,size=(15,1))],
+            [sg.Text('Latest Price', size=(15, 1)), sg.Text(myTicker.latestPrice,size=(15,1))],
+            [sg.Text('52 Week Low', size=(15, 1)), sg.Text(myTicker.week52Low,size=(15,1))],
+            [sg.Text('52 Week High', size=(15, 1)), sg.Text(myTicker.week52High,size=(15,1))],
+            [sg.Text('Total Revenue', size=(15, 1)), sg.Text(myTicker.revenue,size=(15,2))],
             # ****************************
             #       Company Health 
             # ****************************
             [sg.Text('Company Health', size=(45,2), justification='center')],
-            [sg.Text('Net Profit Margin', size=(15, 1)), sg.Text(str(myTicker.netProfitMargin),size=(15,1))],
-            [sg.Text('Total Cash', size=(15, 1)), sg.Text(str(myTicker.cash),size=(15,1))],
-            [sg.Text('Total Debt', size=(15, 1)), sg.Text(str(myTicker.debt),size=(15,2))],
+            [sg.Text('Net Profit Margin', size=(15, 1)), sg.Text(myTicker.netProfitMargin,size=(15,1))],
+            [sg.Text('Total Cash', size=(15, 1)), sg.Text(myTicker.cash,size=(15,1))],
+            [sg.Text('Total Debt', size=(15, 1)), sg.Text(myTicker.debt,size=(15,2))],
             # ****************************
             #           Ratios
             # ****************************
             [sg.Text('Ratios', size=(45,2), justification='center')],
-            [sg.Text('Price To Book', size=(15,1)), sg.Text(str(myTicker.priceToBook),size=(15,1))],
+            [sg.Text('Price To Book', size=(15,1)), sg.Text(myTicker.priceToBook,size=(15,2))],
+            # ****************************
+            #           Stock Health
+            # ****************************
+            [sg.Text('Stock Health', size=(45,2), justification='center')],
+            [sg.Text('EPS', size=(15,1)), sg.Text(myTicker.earningsPerShare,size=(15,2))],
             # ****************************
             #           Uncategorized
             # ****************************
             [sg.Text('Uncategorized', size=(45,2), justification='center')],
-            [sg.Text('Latest Volume', size=(15, 1)), sg.Text(str(myTicker.latestVolume),size=(15,1))],
-            [sg.Text('Dividend Yield', size=(15,1)), sg.Text(str(myTicker.dividendYield),size=(15,1))],
-            [sg.Text('ROE', size=(15,1)), sg.Text(str(myTicker.returnOnEquity),size=(15,1))],
+            [sg.Text('Latest Volume', size=(15, 1)), sg.Text(myTicker.latestVolume,size=(15,1))],
+            [sg.Text('Dividend Yield', size=(15,1)), sg.Text(myTicker.dividendYield,size=(15,1))],
+            [sg.Text('ROE', size=(15,1)), sg.Text(myTicker.returnOnEquity,size=(15,1))],
             [sg.Quit()]  
 ]
 
@@ -47,7 +52,7 @@ TableLayoutWx = [
 #                                       Draw commands 
 #
 # ****************************************************************************************
-window = sg.Window('Company Info',default_element_size=(12,1)).Layout(TableLayoutWx).Finalize()  
+window = sg.Window('Company Info',default_element_size=(12,1)).Layout(TableLayoutWx)  
 
-event, values = window.Read(timeout=None)    
+event, values = window.Read()    
 window.Close()
